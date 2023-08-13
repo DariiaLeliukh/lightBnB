a2abce3204207ab640d9056dd5d365eb77f25f56
DariiaLeliukh <dariialelyukh@gmail.com>
2023-08-12 21:13:40 -0700

    Add functionality to connect db to see all user's reservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -54,7 +54,6 @@ const addUser = function(user) {
   return pool
     .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
     .then((result) => {
-      console.log(result.rows);
       return result.rows[0];
     })
     .catch((err) => {
@@ -70,7 +69,24 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT reservations.*, 
+                properties.*, 
+                avg(rating) as average_rating
+            FROM reservations
+            JOIN properties ON reservations.property_id = properties.id
+            JOIN property_reviews ON property_reviews.id = properties.id
+            WHERE reservations.guest_id = $1
+            GROUP BY reservations.id, properties.id
+            ORDER BY start_date
+            LIMIT $2;`, [guest_id, limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
